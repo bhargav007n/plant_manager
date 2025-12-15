e49f4dcc4377d4d955aec29de7f9a9f60f2b00bb
frappe <frappe@BhargavHP.localdomain>
2025-12-15 14:32:25 +0000

    main

--- plant_manager/plant_manager/doctype/component/component.js
+++ plant_manager/plant_manager/doctype/component/component.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2025, Bhargav N and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Component", {
-// 	refresh(frm) {
+frappe.ui.form.on("Component", {
+	onload: function(frm){
+        frm.set_query( "rm_component", function(){
+            return{
+                'filters':{
+                    "is_rm":1
+                }
+            }
+        }
 
-// 	},
-// });
+        )
+	},
+});
