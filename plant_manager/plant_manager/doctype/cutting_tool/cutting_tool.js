dd0868217b4ba040638ea544d5052daf11ce78e7
bhargav007n <bhargav007n@gmail.com>
2026-01-23 13:42:36 +0000

    main

--- plant_manager/plant_manager/doctype/cutting_tool/cutting_tool.js
+++ plant_manager/plant_manager/doctype/cutting_tool/cutting_tool.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2025, Bhargav N and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Cutting Tool", {
-// 	refresh(frm) {
+frappe.ui.form.on("Cutting Tool", {
+	onload: function(frm) {
+        frm.set_query("prefix", function() {
+        return {
+           "filters": {
+                "cutting_tool_type":frm.doc.cutting_tool_type,
+            }
+        };
+    });
+    },
+    cutting_tool_type:function(frm) {
+        frm.set_value('prefix', '');
+    }
 
-// 	},
-// });
+});
--- plant_manager/plant_manager/doctype/machine_tool_accessory/machine_tool_accessory.js
+++ plant_manager/plant_manager/doctype/machine_tool_accessory/machine_tool_accessory.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2026, Bhargav N and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Machine Tool Accessory", {
-// 	refresh(frm) {
+frappe.ui.form.on("Machine Tool Accessory", {
+	onload: function(frm) {
+        frm.set_query("prefix", function() {
+        return {
+           "filters": {
+                "accessory_type":frm.doc.accessory_type,
+            }
+        };
+    });
+    },
+    accessory_type:function(frm) {
+        frm.set_value('prefix', '');
+    }
 
-// 	},
-// });
+	});
